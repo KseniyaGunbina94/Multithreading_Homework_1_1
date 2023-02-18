d89205196fc7fc4575e9f3fb9926cbf15dd7a611
KseniyaGunbina <senyagunbina@yandex.ru>
2023-02-18 16:11:10 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,84 +1,11 @@
 import java.util.*;
 import java.util.List;
 import java.util.concurrent.*;
-
 public class Main {
-
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        String[] texts = new String[25];
-
-        long startTsWithMultithreading = System.currentTimeMillis(); // start time
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("aab", 30_000);
-        }
-
-        final ExecutorService threadPool = Executors.newFixedThreadPool(25);
-        List<Future<Integer>> threads = new ArrayList<>();
-
-        for (String text : texts) {
-            threads.add(threadPool.submit(() -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-                return maxSize;
-            }));
-        }
-        Integer result = threads.stream()
-                .sorted((o1, o2) -> {
-                    try {
-                        return (o2.get().compareTo(o1.get()));
-                    } catch (InterruptedException | ExecutionException e) {
-                        throw new RuntimeException(e);
-                    }
-                }).findFirst().get().get();
-
-
-        long endTsWithMultithreading = System.currentTimeMillis(); // end time
-        System.out.println("Time: " + (endTsWithMultithreading - startTsWithMultithreading) + "ms\n"+"Result: " + result);
-
-//        long startTs = System.currentTimeMillis(); // start time
-//        for (String text : texts) {
-//            int maxSize = 0;
-//            for (int i = 0; i < text.length(); i++) {
-//                for (int j = 0; j < text.length(); j++) {
-//                    if (i >= j) {
-//                        continue;
-//                    }
-//                    boolean bFound = false;
-//                    for (int k = i; k < j; k++) {
-//                        if (text.charAt(k) == 'b') {
-//                            bFound = true;
-//                            break;
-//                        }
-//                    }
-//                    if (!bFound && maxSize < j - i) {
-//                        maxSize = j - i;
-//                    }
-//                }
-//            }
-//            System.out.println(text.substring(0, 100) + " -> " + maxSize);
-//        }
-//        long endTs = System.currentTimeMillis(); // end time
-//
-//        System.out.println("Time: " + (endTs - startTs) + "ms");
-    }
-
+    public static final int capacity = 100;
+    public static ArrayBlockingQueue<String> arrayBlockingQueueForA = new ArrayBlockingQueue<>(capacity);
+    public static ArrayBlockingQueue<String> arrayBlockingQueueForB = new ArrayBlockingQueue<>(capacity);
+    public static ArrayBlockingQueue<String> arrayBlockingQueueForC = new ArrayBlockingQueue<>(capacity);
     public static String generateText (String letters,int length){
         Random random = new Random();
         StringBuilder text = new StringBuilder();
@@ -86,5 +13,70 @@ public class Main {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
+    }
+    public static int maxCount(char symbol, BlockingQueue<String> blockingQueue) throws ExecutionException, InterruptedException {
+        int max = 0;
+        int count = 0;
+        String text;
+        for (int i = 0; i < 10000; i++) {
+            text = blockingQueue.take();
+            for (char c : text.toCharArray()) {
+                if (c == symbol) count++;
+            }
+            if (count > max) max = count;
+            count = 0;
+        }
+        return max;
+    };
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread textGenerator = new Thread(() -> {
+            for (int i = 0; i < 10_000; i++) {
+                String text = generateText("abc", 100_000);
+                try {
+                    arrayBlockingQueueForA.put(text);
+                    arrayBlockingQueueForB.put(text);
+                    arrayBlockingQueueForC.put(text);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        textGenerator.start();
+
+        Thread aCount = new Thread(() -> {
+            try {
+                int maxCountA = maxCount('a', arrayBlockingQueueForA);
+                System.out.println("Max count of 'A' in all texts -> " + maxCountA);
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        aCount.start();
+
+        Thread bCount = new Thread(() -> {
+            try {
+                int maxCountB = maxCount('b', arrayBlockingQueueForB);
+                System.out.println("Max count of 'B' in all texts -> " + maxCountB);
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        bCount.start();
+
+        Thread cCount = new Thread(() -> {
+            try {
+                int maxCountC = maxCount('c', arrayBlockingQueueForC);
+                System.out.println("Max count of 'C' in all texts -> " + maxCountC);
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        cCount.start();
+
+        aCount.join();
+        bCount.join();
+        cCount.join();
     }
 }
